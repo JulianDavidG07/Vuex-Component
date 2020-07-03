c5560907a1efac18eeb15f41548fdff4ba5d7d09
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-07-02 21:07:34 -0500

    [ADD] tailwind styles

--- src/components/VuexComponent.vue
+++ src/components/VuexComponent.vue
@@ -1,28 +1,35 @@
 <template>
-  <div>
-    <div class="bg-indigo-200 p-4">
-      <ul>
-        <li v-for="(element, index) of elements" :key="element.id">
-          <div class="text-center">
-            {{ element.name }} 
-          </div>         
-          <div class="text-center pb-5">
-            <span class="block">{{ element.amount }}</span>
-            <button class="px-3 py-2 mr-2 rounded-lg focus:outline-none"
-                    :class="[element.amount === 0 ? 'opacity-50 pointer-events-none' : 'bg-red-500 ']"
-                    @click="quitElement(index)"
-                  >-
-            </button>
-
-            <button class="px-3 py-2 rounded-lg focus:outline-none" 
-                    :class="[element.amount === 4 ? 'opacity-50 pointer-events-none' : 'bg-green-500 ']"
-                    @click="addElement(index)"
-                  >+
-            </button>
-          </div>
-        </li>
-      </ul>
+  <div class="flex">
+    <div class="select-none bg-indigo-200 p-4 flex flex-row flex flex-col">
+        <ul>
+          <li v-for="(element, index) of elements" :key="element.id"  class="bg-orange-300 w-24 mb-3 border border-black rounded-lg">
+  
+                <div class="capitalize px-2">{{ element.name }} :</div>
+  
+                <div class="font-bold ml-2 text-center">{{ element.amount }}</div>
+  
+                <div class="p-2 block">
+                  <button class="px-3 py-2 mr-2 rounded-lg focus:outline-none border border-gray-300"
+                          :class="[element.amount === 0 ? 'opacity-50 pointer-events-none' : 'bg-red-500 ']"
+                          @click="quitElement(index)"
+                        >-
+                  </button>
+      
+                  <button class="px-3 py-2 rounded-lg focus:outline-none border border-gray-300" 
+                          :class="[element.amount === 4 ? 'opacity-50 pointer-events-none' : 'bg-green-500 ']"
+                          @click="addElement(index)"
+                        >+
+                  </button>
+                </div>
+  
+          </li>
+        </ul>
     </div>
+  
+    <div>
+      hh
+    </div>
+
   </div>
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    elements: [
+      {id: 1, name: 'water', amount: 0},
+      {id: 2, name: 'air', amount: 0},
+      {id: 3, name: 'land', amount: 0},      
+      {id: 4, name: 'fire', amount: 0}
+    ]
   },
   mutations: {
+    addElement(state, index) {
+      state.elements[index].amount++;
+    },
+
+    quitElement(state, index) {
+      state.elements[index].amount--;
+    }
   },
   actions: {
   },
